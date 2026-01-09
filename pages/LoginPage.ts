96f99398e07c53d32238e5ba49b05d523a824cf4
projas <48687761+projas78@users.noreply.github.com>
2026-01-09 10:13:25 -0300

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -13,20 +13,12 @@ export class LoginPage {
     this.loginBtn = page.locator('[data-test="login-button"]');
   }
 
-  /**
-   * Navega a la página base definida en el config
-   */
   async navigate() {
     await this.page.goto('/');
   }
 
-  /**
-   * Realiza el flujo de login completo
-   * @param user Nombre de usuario
-   * @param pass Contraseña
-   */
   async login(user: string, pass: string) {
-    await this.navigate(); // Llama a la navegación interna
+    await this.navigate();
     await this.userInput.fill(user);
     await this.passInput.fill(pass);
     await this.loginBtn.click();
